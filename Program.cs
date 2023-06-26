4332f3ebba3aa9f3eb21f0f80c4c62badb3ea692
Mohammed Al Sakiti <mohammed110051@gmail.com>
2023-06-26 10:38:23 +0400

    Implement LibraryMember Class

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
